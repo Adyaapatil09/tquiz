074d2a158f092c0d05e4328cc68a3c4a1b561f20
Adyaapatil09 <adyaapatil09@gmail.com>
2022-12-19 05:30:45 +0530

    vresion 2 nightup

--- public/Script.js
+++ public/Script.js
@@ -87,9 +87,45 @@ submit.addEventListener("click", () => {
   if (questionCount < quizDb.length) {
     loadQuestion();
   } else {
-    scorearea.innerHTML = `
-                <h2>you scored ${score}/${quizDb.length} </h2>
-                <button class="btn"> thank you ! <button>
-                `;
+    savescr();
+    gotothanks();
   }
 });
+function gotothanks(){
+  window.location.href="./thanks.html"
+}
+
+function savescr() {
+  localStorage.setItem("score", score);
+}
+
+
+//timer 
+document.getElementById("clock").innerHTML = 30 + ":" + 01;
+startTimer();
+
+function startTimer() {
+    var presentTime = document.getElementById("clock").innerHTML;
+    var timeArray = presentTime.split(/[:]+/);
+    var m = timeArray[0];
+    var s = checkSecond(timeArray[1] - 1);
+    if (s == 59) {
+        m = m - 1;
+    }
+    if (m < 0) {
+        return;
+    }
+
+    document.getElementById("clock").innerHTML = m + ":" + s;
+    setTimeout(startTimer, 1000);
+}
+
+function checkSecond(sec) {
+    if (sec < 10 && sec >= 0) {
+        sec = "0" + sec;
+    } // add zero in front of numbers < 10
+    if (sec < 0) {
+        sec = "59";
+    }
+    return sec;
+}
